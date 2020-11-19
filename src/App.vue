0f15ae3fa6a61725ba269b78a19c3e2dce21203b
melvinDebot <melvindebot@gmail.com>
2020-11-19 15:26:10 +0100

    add font

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
 #app {
   width: 100%;
   height: 100vh;
+  font-family: 'Poppins', sans-serif;
 }
 </style>
