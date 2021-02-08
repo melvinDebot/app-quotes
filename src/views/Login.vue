cc6a1afe6446f59eeb3ba138b5aa2d2780d26407
Melvin Debot <melvin@exceenis.com>
2021-02-08 14:27:50 +0100

    style: page login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="login">
-    <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email" /><br />
-    <input type="password" v-model="password" placeholder="Password" /><br />
+    <div class="banner">
+      <h2>Se connecter</h2>
+    </div>
+    <div class="login--input">
+      <h4>Email</h4>
+      <input type="text" v-model="email" placeholder="Email" />
+    </div>
+    <div class="login--input">
+      <h4>Mot de passe</h4>
+      <input type="password" v-model="password" placeholder="Password" />
+    </div>
+    <button class="login--button" @click="login">
+      <h4>Me connecter</h4>
+    </button>
+    <button class="login--button google" @click="login">
+      <h4>Me connecter avec google</h4>
+    </button>
+    <router-link to="/sign-up">
+      <h5>Nouvel Utilisateur ? <span>Sinscrire</span></h5>
+    </router-link>
+    
+    <!-- 
     <button @click="login">Connection</button>
     <p>
       or Sign In with Google <br />
@@ -11,7 +30,7 @@
     <p>
       You don't have an account ? You can
       <router-link to="/sign-up">create one</router-link>
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -53,42 +72,90 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 /* "scoped" attribute limit the CSS to this component only */
 .login {
-  margin-top: 40px;
-}
-input {
-  margin: 10px 0;
-  width: 20%;
-  padding: 15px;
-}
-button {
-  margin-top: 20px;
-  width: 10%;
-  cursor: pointer;
-}
-p {
-  margin-top: 40px;
-  font-size: 13px;
-}
-p a {
-  text-decoration: underline;
-  cursor: pointer;
-}
-.social-button {
-  width: 75px;
-  background: white;
-  padding: 10px;
-  border-radius: 100%;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-  outline: 0;
-  border: 0;
-}
-.social-button:active {
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-}
-.social-button img {
   width: 100%;
+  height: 100%;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0;
+  .banner {
+    width: 100%;
+    height: 113px;
+    background: #ffc61b;
+    border-radius: 0px 0px 15px 15px;
+    position: relative;
+    text-align: center;
+    padding-top: 50px;
+    box-shadow: 0px 10px 0px #FBE193;
+    color: white;
+  }
+  .login--input{
+    width: 85%;
+    height: 75px;
+    &:nth-child(1){
+      margin-top: 40px;
+    }
+    margin-top: 25px;
+    input {
+      width: 100%;
+      padding: 10px 5px;
+      margin-top: 10px;
+    }
+  }
+  .login--button{
+    width: 70%;
+    height: 53px;
+    background: #FFC61B;
+    border: none;
+    color: white;
+    text-align: center;
+  }
+  .google{
+    background: #046CF4;
+  }
+  h5{
+    margin-top: 30px;
+    font-weight: 400;
+    span{
+      font-weight: 600;
+      color: #FFC61B;
+    }
+  }
+  a {
+    text-decoration: none;
+    cursor: pointer;
+    color: black;
+  }
+  
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    
+    font-size: 13px;
+    
+    .social-button {
+      width: 75px;
+      background: white;
+      padding: 10px;
+      border-radius: 100%;
+      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+      outline: 0;
+      border: 0;
+      &:active {
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
